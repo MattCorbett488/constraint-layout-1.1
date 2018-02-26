1600f905ed1f61cb2ac7b631f0e7800eb4656e4b
Matt Corbett <matthew.corbett@willowtreeapps.com>
2018-02-25 21:10:13 -0500

    Add fun stuff

--- app/src/main/java/com/example/matthewcorbett/constraintlayout11/CircularConstraintActivity.kt
+++ app/src/main/java/com/example/matthewcorbett/constraintlayout11/CircularConstraintActivity.kt
@@ -1,33 +1,57 @@
 package com.example.matthewcorbett.constraintlayout11
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.AppCompatImageView
 import android.view.View
 import kotlinx.android.synthetic.main.activity_circular_constraints.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class CircularConstraintActivity : AppCompatActivity() {
-    private val RADIUS_INCREMENT_DEFAULT by lazy { resources.getDimensionPixelOffset(R.dimen.radius_increase_default) }
-    private val ANGLE_INCREMENT_DEFAULT by lazy { resources.getInteger(R.integer.angle_increase_default) }
+    private val radiusIncrement by lazy { resources.getDimensionPixelOffset(R.dimen.radius_increase_default) }
+    private val angleIncrement by lazy { resources.getInteger(R.integer.angle_increase_default) }
+
+    private val random = Random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_circular_constraints)
 
-        incrementInnerAngle.setOnClickListener { incrementAngle(innerElement, ANGLE_INCREMENT_DEFAULT) }
+        initFaces()
+        setListeners()
+    }
 
-        incrementOuterAngle.setOnClickListener { incrementAngle(outerElement, ANGLE_INCREMENT_DEFAULT) }
+    private fun initFaces() {
+        val facesList = arrayListOf(R.mipmap.ic_adam, R.mipmap.ic_eric, R.mipmap.ic_matt)
+        centerElement.setImageDrawable(getFace(facesList))
+        innerElement.setImageDrawable(getFace(facesList))
+        outerElement.setImageDrawable(getFace(facesList))
+    }
 
-        incrementInnerRadius.setOnClickListener { incrementRadius(innerElement, RADIUS_INCREMENT_DEFAULT) }
+    private fun getFace(faces: ArrayList<Int>): Drawable {
+        val face = faces[random.nextInt(faces.size)]
+        faces.remove(face)
+        return ContextCompat.getDrawable(this, face)
+    }
 
-        incrementOuterRadius.setOnClickListener { incrementRadius(outerElement, RADIUS_INCREMENT_DEFAULT) }
+    private fun setListeners() {
+        incrementInnerAngle.setOnClickListener { incrementAngle(innerElement, angleIncrement) }
+
+        incrementOuterAngle.setOnClickListener { incrementAngle(outerElement, angleIncrement) }
+
+        incrementInnerRadius.setOnClickListener { incrementRadius(innerElement, radiusIncrement) }
+
+        incrementOuterRadius.setOnClickListener { incrementRadius(outerElement, radiusIncrement) }
 
         reset.setOnClickListener {
             reset(innerElement, R.dimen.inner_radius)
             reset(outerElement, R.dimen.outer_radius)
         }
     }
-
     private fun incrementAngle(view: View, angleIncrement: Int) {
         val params = view.layoutParams as ConstraintLayout.LayoutParams
         params.circleAngle = (params.circleAngle + angleIncrement) % 360
