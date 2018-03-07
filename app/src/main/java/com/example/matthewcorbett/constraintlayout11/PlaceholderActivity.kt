e7586b6f4273568a6a0a437f1dfc6a79008c3d2f
Matt Corbett <matthew.corbett@willowtreeapps.com>
2018-03-07 15:24:16 -0500

    Update drawables for versions below 24
    Updated drawables so we aren't seeing crashes on devices below 24.  Also updated the Toast message to show in the center of the screen

--- app/src/main/java/com/example/matthewcorbett/constraintlayout11/PlaceholderActivity.kt
+++ app/src/main/java/com/example/matthewcorbett/constraintlayout11/PlaceholderActivity.kt
@@ -1,32 +1,46 @@
 package com.example.matthewcorbett.constraintlayout11
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.transition.ChangeBounds
 import android.transition.TransitionManager
+import android.view.Gravity.CENTER
 import android.view.View
+import android.view.animation.OvershootInterpolator
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 import kotlinx.android.synthetic.main.activity_placeholder.*
 import kotlinx.android.synthetic.main.include_placeholder.*
 
+@SuppressLint("ShowToast")
 class PlaceholderActivity : AppCompatActivity() {
+    private val toast by lazy { Toast.makeText(this, "", LENGTH_SHORT) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placeholder)
 
-        firstFace.onClick { Toast.makeText(this, "Here come Dat Boi!", LENGTH_SHORT).show() }
+        toast.setGravity(CENTER, 0, 0)
+
+        firstFace.onClick { toast.showWithText(R.string.here_come) }
 
         secondFace.onClick { }
 
-        thirdFace.onClick { Toast.makeText(this, "Gotta go fast", LENGTH_SHORT).show() }
+        thirdFace.onClick { toast.showWithText(R.string.go_fast) }
     }
 
-    private fun View.onClick(func: () -> Unit) {
-        setOnClickListener {
-            TransitionManager.beginDelayedTransition(root)
-            func()
-            placeholderFace.setContentId(this.id)
-        }
+    private inline fun View.onClick(crossinline func: () -> Unit) = setOnClickListener {
+        func()
+        placeholderFace.setContentId(this.id)
+        TransitionManager.beginDelayedTransition(root, ChangeBounds().apply {
+            duration = 1000
+            interpolator = OvershootInterpolator()
+        })
+    }
+
+    private fun Toast.showWithText(messageRes: Int) = this.apply {
+        setText(messageRes)
+        show()
     }
 }
