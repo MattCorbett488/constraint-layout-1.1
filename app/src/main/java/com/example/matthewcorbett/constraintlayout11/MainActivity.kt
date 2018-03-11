c35d4c20bed2506c8d29c8eaddfd303cf7e597cb
Matt Corbett <matthew.corbett@willowtreeapps.com>
2018-03-11 17:35:01 -0400

    Add intent listener method for main activity

--- app/src/main/java/com/example/matthewcorbett/constraintlayout11/MainActivity.kt
+++ app/src/main/java/com/example/matthewcorbett/constraintlayout11/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.matthewcorbett.constraintlayout11
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.AppCompatButton
 import android.view.Window
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,26 +15,15 @@ class MainActivity : AppCompatActivity() {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
 
-        barrierGuidelineButton.setOnClickListener {
-            startActivity(Intent(this, BarrierActivity::class.java))
-        }
-        circleConstraintButton.setOnClickListener {
-            startActivity(Intent(this, CircularConstraintActivity::class.java))
-        }
-        percentRatioButton.setOnClickListener {
-            startActivity(Intent(this, DimensionConstraintActivity::class.java))
-        }
+        barrierGuidelineButton.setIntentListener(BarrierActivity::class.java)
+        circleConstraintButton.setIntentListener(CircularConstraintActivity::class.java)
+        percentRatioButton.setIntentListener(DimensionConstraintActivity::class.java)
+        placeholderButton.setIntentListener(PlaceholderActivity::class.java)
+        placeholderRotationButton.setIntentListener(PlaceholderRotationActivity::class.java)
+        groupButton.setIntentListener(GroupActivity::class.java)
+    }
 
-        placeholderButton.setOnClickListener {
-            startActivity(Intent(this, PlaceholderActivity::class.java))
-        }
-
-        placeholderRotationButton.setOnClickListener {
-            startActivity(Intent(this, PlaceholderRotationActivity::class.java))
-        }
-
-        groupButton.setOnClickListener {
-            startActivity(Intent(this, GroupActivity::class.java))
-        }
+    private fun AppCompatButton.setIntentListener(clazz: Class<out AppCompatActivity>) = setOnClickListener {
+        startActivity(Intent(this@MainActivity, clazz))
     }
 }
